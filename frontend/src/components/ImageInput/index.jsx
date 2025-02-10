1f9134fc36462f1bc60f17fda4b15226cc6f70ed
luisfabianodcomp <fabiano.leite@dcomp.ufs.br>
2025-02-10 09:31:03 -0300

    add feature to delete image input

--- frontend/src/components/ImageInput/index.jsx
+++ frontend/src/components/ImageInput/index.jsx
@@ -1,11 +1,10 @@
+import './styles.css'
 import React, { useState, useRef } from 'react'
+import closeIcon from '../../assets/images/x.png'
 
 export const ImageInput = ({ isErrorMessageShaking, setTotalImages, totalImages, index }) => {
-
     const imagemProdutoRef = useRef(null)
-
     const [image, setImage] = useState(null);
-
     const [isFirstTime, setIsFirstTime] = useState(true);
 
     const handleInput = (event) => {
@@ -15,23 +14,46 @@ export const ImageInput = ({ isErrorMessageShaking, setTotalImages, totalImages,
         }
 
         const file = event.target.files[0];
-        const reader = new FileReader();
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setImage(reader.result);
+            };
+            reader.readAsDataURL(file);
+        }
+    }
 
-        reader.onloadend = () => {
-            setImage(reader.result);
-        };
+    const handleClick = () => {
+        setImage(null); 
+        if (imagemProdutoRef.current) {
+            imagemProdutoRef.current.value = ""; 
+        }
 
-        reader.readAsDataURL(file);
+        // Removendo do total de imagens
+        const updatedImages = [...totalImages];
+        updatedImages.splice(index, 1);
+        setTotalImages(updatedImages);
+        setIsFirstTime(true); 
     }
 
     return (
-            <div className='nome image-input'>
+        <div className='nome image-input'>
+            <header className='img-input-header'>
                 <label htmlFor={`imagem${index + 1}`}>*Imagem {index + 1}</label>
-                <img src={image} alt='img-preview' />
-                <input type='file' name='imagem1' ref={imagemProdutoRef} onInput={handleInput}></input>
-                
-                <span className={`error-message ${isErrorMessageShaking ? 'shake' : ''}`}></span>
-                
-            </div>
+                {image && (
+                    <img
+                        src={closeIcon}
+                        alt="delete-img-icon"
+                        className='delete-img-icon'
+                        onClick={handleClick}
+                    />
+                )}
+            </header>
+            
+            {image && <img src={image} alt='img-preview' />}
+            <input type='file' name={`imagem${index + 1}`} ref={imagemProdutoRef} onInput={handleInput} />
+
+            <span className={`error-message ${isErrorMessageShaking ? 'shake' : ''}`}></span>
+        </div>
     )
 }
